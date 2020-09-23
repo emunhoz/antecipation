47da4123836cac1de3b777ed0aac7123e608949d
emunhoz <edermunhozsantos@gmail.com>
2020-09-23 15:28:41 -0300

    feat: register custom components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles/global.scss";
+import BaseCard from "./components/base-card";
+import BaseInput from "./components/base-input";
+
+import "./styles/global.css";
+
+customElements.define("base-card", BaseCard);
+customElements.define("base-input", BaseInput);
