79d2d2110a6807c69144e99ee4bf6c21222cc6da
emunhoz <edermunhozsantos@gmail.com>
2020-09-24 01:24:55 -0300

    feat: add base card component

--- src/components/base-card/index.js
+++ src/components/base-card/index.js
@@ -1,29 +1,29 @@
-const template = document.createElement("template");
-template.innerHTML = `
-  <style>
-    .base-card {
-      margin: 0 auto;
-      background: var(--light);
-      border: 1px solid #d1dce3;
-      box-sizing: border-box;
-      border-radius: 4px;
-      padding: 41px 56px 44px;
-      max-width: 608px;
-      width: 100%;
-    }
-  </style>
-  <div class="base-card">
-    <slot></slot>
-  </div>
-`;
-
-class BaseCard extends HTMLElement {
+export default class BaseCard extends HTMLElement {
   constructor() {
     super();
-    const shadowDOM = this.attachShadow({ mode: "open" });
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
 
-    shadowDOM.appendChild(template.content.cloneNode(true));
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadow.innerHTML = `
+    <style>
+      .base-card {
+        margin: 0 auto;
+        background: var(--light);
+        border: 1px solid #d1dce3;
+        box-sizing: border-box;
+        border-radius: 4px;
+        max-width: 608px;
+        width: 100%;
+      }
+    </style>
+    <div class="base-card">
+      <slot></slot>
+    </div>
+    `
   }
 }
-
-export default BaseCard;
