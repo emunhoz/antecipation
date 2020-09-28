22cc71fe1501ada70abbd9542f66957e00cd7064
emunhoz <edermunhozsantos@gmail.com>
2020-09-28 03:31:56 -0300

    chore: register custom elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import "./styles/global.css"
+import HomePage from './pages/home'
+
 import BaseCard from "./components/base-card";
 import BaseInput from "./components/base-input";
+import ListPreviewValues from "./components/list-preview-values";
+import LoadingEl from "./components/loading-el";
 
-import "./styles/global.css";
+// Pages
+customElements.define("home-page", HomePage);
 
+// Components
 customElements.define("base-card", BaseCard);
 customElements.define("base-input", BaseInput);
+customElements.define("list-preview-values", ListPreviewValues);
+customElements.define("loading-el", LoadingEl);
