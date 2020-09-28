bf0bafd8a5d40d89c1a491a3ec8af0350ea61b95
emunhoz <edermunhozsantos@gmail.com>
2020-09-28 03:27:01 -0300

    refactor: change attrs input element

--- src/components/base-input/index.js
+++ src/components/base-input/index.js
@@ -1,4 +1,5 @@
-import formatCurrency from '../../utils/formatCurrency'
+import { formatCurrency } from '../../utils/formatCurrency'
+import kebabCase from '../../utils/kebabCase'
 
 export default class BaseInput extends HTMLElement {
   constructor() {
@@ -26,21 +27,30 @@ export default class BaseInput extends HTMLElement {
     return this.getAttribute('max');
   }
 
+  get min() {
+    return this.getAttribute('min');
+  }
+
   get maxLength() {
     return this.getAttribute('maxLength');
-  }  
+  }
+
+  formatValue(e) {
+    return e.target.value = formatCurrency(e.target.value)
+  }
 
   connectedCallback() {
     this.render();
 
     let inputWithMaskMoney = this.shadow.querySelector("[data-mask='money']");
-    inputWithMaskMoney && inputWithMaskMoney.addEventListener('input', e => e.target.value = formatCurrency(e.target.value), false);
+    inputWithMaskMoney && inputWithMaskMoney.addEventListener('input', this.formatValue.bind(this), false);
   }
 
   render() {
-    const { label, type, helpMessage, mask, max, maxLength } = this
+    const { label, type, helpMessage, mask, max, min, maxLength } = this
 
     const maxAttr = max && `max="${max}"` || '';
+    const minAttr = min && `min="${min}"` || '';
     const maxLengthAttr = maxLength && `maxlength="${maxLength}"` || '';
 
     this.shadow.innerHTML = `
@@ -72,8 +82,9 @@ export default class BaseInput extends HTMLElement {
       }
     </style>
     <div class="base-input">
-      <label for="${label}">${label}</label>
-      <input type="${type}" ${maxAttr} ${maxLengthAttr} data-mask="${mask}" id="${label}" />
+
+      <label for="${kebabCase(label)}">${label}</label>
+      <input type="${type}" ${minAttr} ${maxAttr} ${maxLengthAttr} data-mask="${mask}" id="${kebabCase(label)}" />
       <small class="help-message">${helpMessage}</small>
     </div>
     `
