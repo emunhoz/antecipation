8da5fce2362e8e0cbf5efd1fa55209dc5b55fb68
emunhoz <edermunhozsantos@gmail.com>
2020-09-28 03:24:07 -0300

    feat: add mask currency function

--- src/utils/formatCurrency/formatCurrency.test.js
+++ src/utils/formatCurrency/formatCurrency.test.js
@@ -1,4 +1,13 @@
-import formatCurrency from './';
+import { maskCurrency, formatCurrency } from './';
+
+test('should return empty string when user input an invalid value', () => {
+  expect(maskCurrency('number')).toBe('');
+  expect(maskCurrency(undefined)).toBe('');
+});
+
+test('show a masked value', () => {
+  expect(maskCurrency(1000)).toBe('R$1,000.00');
+});
 
 test('should return zero value when user input is empty', () => {
   expect(formatCurrency('')).toBe("R$0.00");
--- src/utils/formatCurrency/index.js
+++ src/utils/formatCurrency/index.js
@@ -1,4 +1,14 @@
-export default function formatCurrency (value) {
+export function maskCurrency(number) {
+  if (Number.isNaN(parseFloat(number)) && Number.isNaN(number - 0)) return '';
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(number);
+}
+
+export function formatCurrency (value) {
   const val = +value.replace(/\D+/g, '') / 100;
 
   return new Intl.NumberFormat('pt-BR', {
@@ -6,4 +16,3 @@ export default function formatCurrency (value) {
     currency: 'BRL',
   }).format(val);
 };
-
