aa0e70c285fac26c4f2bfd0afc3241f9f62f56e3
emunhoz <edermunhozsantos@gmail.com>
2020-09-29 01:56:37 -0300

    chore: remove sw register

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import BaseCard from "./components/base-card";
 import BaseInput from "./components/base-input";
 import ListPreviewValues from "./components/list-preview-values";
 import LoadingEl from "./components/loading-el";
+import AlertMessage from "./components/alert-message";
 
 // Pages
 customElements.define("home-page", HomePage);
@@ -14,12 +15,4 @@ customElements.define("base-card", BaseCard);
 customElements.define("base-input", BaseInput);
 customElements.define("list-preview-values", ListPreviewValues);
 customElements.define("loading-el", LoadingEl);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-      navigator.serviceWorker
-          .register('../sw.js')
-          .then(res => console.log("service worker registered"))
-          .catch(err => console.log("service worker not registered", err))
-  })
-}
+customElements.define("alert-message", AlertMessage);
